70a4c145de9d18ac60572bea113144601eca64e6
JustasMit <segjustas12@gmail.com>
2021-11-04 08:27:18 +0200

    working

--- additional_criteria.py
+++ additional_criteria.py
@@ -10,13 +10,11 @@ def by_date(layer, date):
 
 def by_type(layer, type, output):
     if type == "Sukurtas":
-        return arcpy.MakeFeatureLayer_management(layer, type, "created_date IS NOT NULL") # daug created_date yra null. sitas buvo nelogiskas: last_edited_date Or (last_edited_date IS NOT NULL And created_date IS NULL)")
+        return arcpy.MakeFeatureLayer_management(layer, type, "created_date IS NOT NULL and created_date = last_edited_date")
     elif type == "Redaguotas":
         return arcpy.MakeFeatureLayer_management(layer, type, "created_date <> last_edited_date")
     elif type == "Sukurtas arba redaguotas":
-        created = arcpy.MakeFeatureLayer_management(layer, "Created_Temp", "created_date IS NOT NULL")
-        edited = arcpy.MakeFeatureLayer_management(layer, "Edited_Temp", "created_date <> last_edited_date")
-        return arcpy.Merge_management([created, edited], output)
+        return arcpy.MakeFeatureLayer_management(layer, type, "(created_date IS NOT NULL and created_date = last_edited_date) or created_date <> last_edited_date")
 
 def by_teritory(layer, teritory, output):
     arcpy.MakeFeatureLayer_management("https://services1.arcgis.com/usA3lHW20rGU6glp/ArcGIS/rest/services/Zenklu_prieziuros_teritorijos_view/FeatureServer/0", "Teritory")
