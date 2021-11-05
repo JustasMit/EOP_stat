4144da60202e8046e84dd971d9949e2a6d5f82fd
JustasMit <segjustas12@gmail.com>
2021-11-05 08:45:36 +0200

    remove module

--- INFRtool.py
+++ INFRtool.py
@@ -1,6 +1,24 @@
 import arcpy
 import os
-from additional_criteria import *
+
+def by_date(layer, date):
+    return arcpy.MakeFeatureLayer_management(layer, "Date_Temp", "last_edited_date > '{}'".format(date))
+
+def by_type(layer, type, output):
+    if type == "Sukurtas":
+        return arcpy.MakeFeatureLayer_management(layer, type, "created_date IS NOT NULL and created_date = last_edited_date")
+    elif type == "Redaguotas":
+        return arcpy.MakeFeatureLayer_management(layer, type, "created_date <> last_edited_date")
+    elif type == "Sukurtas arba redaguotas":
+        return arcpy.MakeFeatureLayer_management(layer, type, "(created_date IS NOT NULL and created_date = last_edited_date) or created_date <> last_edited_date")
+
+def by_teritory(layer, teritory, output):
+    arcpy.MakeFeatureLayer_management("https://services1.arcgis.com/usA3lHW20rGU6glp/ArcGIS/rest/services/Zenklu_prieziuros_teritorijos_view/FeatureServer/0", "Teritory")
+    if teritory != "Visa teritorija":
+        arcpy.MakeFeatureLayer_management("Teritory", teritory, "SENIUNIJA = '{}'".format(teritory))
+        return arcpy.analysis.Clip(layer, teritory, output, None)
+    else:
+        return arcpy.analysis.Clip(layer, "Teritory", output, None)
 
 def infr_line(infr):
     length = 0
@@ -29,8 +47,6 @@ def infr_stats(infr):
         return infr_polygon(infr)
     elif shape == "Point":
         return infr_point(infr)
-
-
 
 scratchGDB = arcpy.env.scratchGDB
 out_featureclass = os.path.join(scratchGDB, "INFR_TEMP")  
--- KZtool.py
+++ KZtool.py
@@ -1,6 +1,24 @@
 import arcpy
 import os
-from additional_criteria import *
+
+def by_date(layer, date):
+    return arcpy.MakeFeatureLayer_management(layer, "Date_Temp", "last_edited_date > '{}'".format(date))
+
+def by_type(layer, type, output):
+    if type == "Sukurtas":
+        return arcpy.MakeFeatureLayer_management(layer, type, "created_date IS NOT NULL and created_date = last_edited_date")
+    elif type == "Redaguotas":
+        return arcpy.MakeFeatureLayer_management(layer, type, "created_date <> last_edited_date")
+    elif type == "Sukurtas arba redaguotas":
+        return arcpy.MakeFeatureLayer_management(layer, type, "(created_date IS NOT NULL and created_date = last_edited_date) or created_date <> last_edited_date")
+
+def by_teritory(layer, teritory, output):
+    arcpy.MakeFeatureLayer_management("https://services1.arcgis.com/usA3lHW20rGU6glp/ArcGIS/rest/services/Zenklu_prieziuros_teritorijos_view/FeatureServer/0", "Teritory")
+    if teritory != "Visa teritorija":
+        arcpy.MakeFeatureLayer_management("Teritory", teritory, "SENIUNIJA = '{}'".format(teritory))
+        return arcpy.analysis.Clip(layer, teritory, output, None)
+    else:
+        return arcpy.analysis.Clip(layer, "Teritory", output, None)
 
 def kz_stats(kz):
     count = arcpy.GetCount_management(kz)
